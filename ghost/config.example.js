7860d3b641d553c6a2f12de5e83a13aa273deaf3
Andrea Usuelli <andreausu@gmail.com>
2013-10-21 20:19:36 +0200

    Upgraded ghost to 0.3.3

--- ghost/config.example.js
+++ ghost/config.example.js
@@ -8,7 +8,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://localhost:2368',
+        url: 'http://my-ghost-blog.com',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
